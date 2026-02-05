0278e43d20c62047a6f450d341a0f8d2da89798b
pdah2626 <pdah2626@uni.sydney.edu.au>
2026-02-05 13:14:41 +1100

    added a get route

--- code-reviewer/routes/app.routes.js
+++ code-reviewer/routes/app.routes.js
@@ -3,5 +3,6 @@ const appRouter = express.Router();
 const {getResponse} = require('../controller/app.controller');
 
 appRouter.post("/review", getResponse);
+appRouter.Router.get('/', (req, res)=>(res.send("backend is running")));
 
 module.exports = appRouter;
