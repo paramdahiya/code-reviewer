64ff6e7b064e6690cfb75ada132fa31ff06f22d9
pdah2626 <pdah2626@uni.sydney.edu.au>
2026-02-05 13:21:56 +1100

    added a root get route in app.js

--- code-reviewer/routes/app.routes.js
+++ code-reviewer/routes/app.routes.js
@@ -3,6 +3,4 @@ const appRouter = express.Router();
 const {getResponse} = require('../controller/app.controller');
 
 appRouter.post("/review", getResponse);
-appRouter.get('/', (req, res)=>(res.send("backend is running")));
-
 module.exports = appRouter;
--- code-reviewer/src/app.js
+++ code-reviewer/src/app.js
@@ -7,6 +7,9 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res)=>(res.send("backend is running")));
+
+
 app.use('/api', appRouter);
 
 module.exports = app;
